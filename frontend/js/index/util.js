6708ff49e0254d81e47616fafe6b5f624150b1a3
taiprovip3 <taito1doraemon@gmail.com>
2024-07-28 23:59:09 +0700

    feat/fix search from genres menu

--- frontend/js/index/util.js
+++ frontend/js/index/util.js
@@ -26,10 +26,3 @@ function updateCountdown() {
         clearInterval(interval);
     }
 }
-
-function searchBooks() {
-    const keySearchBook = $('#keySearchBook').val();
-    if (keySearchBook) {
-        window.location.href = './search.html?key=' + encodeURIComponent(keySearchBook);
-    }
-}
